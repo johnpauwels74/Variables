4c45b35ccbe0875159052ce745f253c8fc585d42
johnpauwels74 <johnpauwels74@gmail.com>
2017-09-06 10:11:01 -0500

    Initial commit

---
+++ src/com/Variables/java/Main.java
@@ -0,0 +1,24 @@
+package com.Variables.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte bytevar = 1;
+	short shortvar = 4;
+	int intvar = 12;
+	long longvar = 239524;
+	float floatvar = 1.758984f;
+    double doublevar = 34235.34657;
+    boolean booleanvar = true;
+    char charvar = 'k';
+
+        System.out.println("byte data type: " + bytevar);
+        System.out.println("short data type: " + shortvar);
+        System.out.println("int data type: " + intvar);
+        System.out.println("long data type: " + longvar);
+        System.out.println("float data type: " + floatvar);
+        System.out.println("double data type: " + doublevar);
+        System.out.println("boolean data type: " + booleanvar);
+        System.out.println("char data type: " + charvar);
+    }
+}
